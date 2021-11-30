e545c4635bde707f73ae7770f246f29a22ff22c4
ali abbani <aliox@ahlam.com>
2021-11-30 11:51:39 +0200

    generate controller Pages hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'pages/hello'
+  root 'pages#hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
